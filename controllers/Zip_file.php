1cfc02bd983bf3285d9521517aa5e0d89ad7cb53
AISHWARYA <113532088+aishwarya-art@users.noreply.github.com>
2023-10-04 16:20:50 +0530

    Create Zip_file.php

---
+++ controllers/Zip_file.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Zip_file extends CI_Controller {
+ 
+ function index()
+ {
+   $this->load->helper('url');
+  $directory = 'download';
+  $data["images"] = glob($directory . "/*.jpg");
+  $this->load->view('zip_file_view', $data);
+ }
+
+ function download()
+ {
+  if($this->input->post('images'))
+  {
+   $this->load->library('zip');
+   $images = $this->input->post('images');
+   foreach($images as $image)
+   {
+    $this->zip->read_file($image);
+   }
+   $this->zip->download(''.time().'.zip');
+  }
+ }
+
+}
+
+?>
